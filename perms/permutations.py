cacd4f864c7a2b550ac25cf8f35d90d6530c3c6e
Nick Laity <44630816+NickatGIHS@users.noreply.github.com>
2019-03-20 16:45:59 +1030

    Signing off

--- perms/permutations.py
+++ perms/permutations.py
@@ -2,7 +2,8 @@ def numOfPerm(string):
     """Input a [string] into this function and it will
        output the number of different combinations that
        are possible to make with it. Assumes that there
-       are no double-ups of letters. Returns an integer."""
+       are no double-ups of letters. Returns an integer.
+        - By GIHS RPC"""
     string = str(string)
     multip = [1]
     ans = 1
