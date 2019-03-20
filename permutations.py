3a5c8891da4fabfd0d44b277e6b769f2c38a48b1
Nick Laity <44630816+NickatGIHS@users.noreply.github.com>
2019-03-20 16:35:33 +1030

    Added permutations: python version
    Maybe we could do more languages?

---
+++ permutations.py
@@ -0,0 +1,13 @@
+def numOfPerm(string):
+    """Input a [string] into this function and it will
+       output the number of different combinations that
+       are possible to make with it. Assumes that there
+       are no double-ups of letters. Returns an integer."""
+    string = str(string)
+    multip = [1]
+    ans = 1
+    for i in range(len(string)):
+        multip.append((len(string)-i))
+    for thing in multip:
+        ans = ans*thing
+    return ans
